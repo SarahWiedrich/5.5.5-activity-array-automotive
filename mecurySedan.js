4a0019e151cd8524165f0968aa93708a6af1d498
Sarah Wiedrich <sarahwiedrich@Sarahs-MBP.Home>
2022-04-01 15:00:55 -0500

    activity started, template written for class and methods

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,31 @@ const VehicleModule = require("./vehicle")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+//A28 Mercury Sedan Class
+class mercuryA28 {
+    constructor(maxPassengers, passenger, numOfWheels, maxSpeed, fuel, schService){
+        this.maxPassengers = maxPassengers;
+        this.passenger = passenger;
+        this.numOfWheels = numOfWheels;
+        this.maxSpeed = maxSpeed;
+        this.fuel = fuel;
+        this.schService = schService;
+    }
+    //methods
+    loadPassenger(num){
+        if (this.passenger < this.maxPassengers) 
+        return avaliableRoom === true
+    }
+
+    start(){
+        if(this.fuel > 0)
+        return start === true
+    }
+
+    scheduledService(mileage){
+        if(mileage > 30000)
+        return timeForMaintence === true
+    }
+
+}
